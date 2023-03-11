f3c5f055566dbc2a1209e356722a3d9daf8631f3
abhinandan mohanty <113935684+ABHINANDAN-CODER@users.noreply.github.com>
2023-03-11 13:14:26 +0530

    complete

--- main.js
+++ main.js
@@ -1,6 +1,70 @@
-function myFunction(tg){
-      var target = document.getElementById('tg');
-      target.style.visibility='hidden';
-      alert("CLICKED");
-      
+function start(){
+      var target = document.getElementById('f');
+      target.classList.add('show');
+}
+function f(){
+      var source = document.getElementById('f');
+      source.classList.remove('show');
+      var target = document.getElementById('l');
+      target.classList.add('show');
+}
+function l(){
+      var source = document.getElementById('l');
+      source.classList.remove('show');
+      var target = document.getElementById('e');
+      target.classList.add('show');
+}
+function e(){
+      var source = document.getElementById('e');
+      source.classList.remove('show');
+      var target = document.getElementById('h');
+      target.classList.add('show');
+}
+function h(){
+      var source = document.getElementById('h');
+      source.classList.remove('show');
+      var target = document.getElementById('g');
+      target.classList.add('show');
+}
+function g(){
+      var source = document.getElementById('g');
+      source.classList.remove('show');
+      var target = document.getElementById('a');
+      target.classList.add('show');
+}
+function a(){
+      var source = document.getElementById('a');
+      source.classList.remove('show');
+      var target = document.getElementById('k');
+      target.classList.add('show');
+}
+function k(){
+      var source = document.getElementById('k');
+      source.classList.remove('show');
+      var target = document.getElementById('c');
+      target.classList.add('show');
+}
+function c(){
+      var source = document.getElementById('c');
+      source.classList.remove('show');
+      var target = document.getElementById('i');
+      target.classList.add('show');
+}
+function i(){
+      var source = document.getElementById('i');
+      source.classList.remove('show');
+      var target = document.getElementById('b');
+      target.classList.add('show');
+}
+function b(){
+      var source = document.getElementById('b');
+      source.classList.remove('show');
+      var target = document.getElementById('j');
+      target.classList.add('show');
+}
+function j(){
+      var source = document.getElementById('j');
+      source.classList.remove('show');
+      var target = document.getElementById('f');
+      target.classList.add('show');
 }
